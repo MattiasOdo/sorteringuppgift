b98e4569334d9f98f64f22d010453e7f9b6e723f
MattiasOdo <Mattias.odo@utb.helsingborg.se>
2020-03-26 09:15:50 +0100

    Sorteringsuppgift

--- Bubblesort/Program.cs
+++ Bubblesort/Program.cs
@@ -11,11 +11,12 @@ namespace Bubblesort
         static void Bubblesort(List<int> sortlist)
         {
             bool listsorted = false;
-
+            int j = 0;
             while (listsorted == false)
             {
+
                 listsorted = true;
-                for (int i = 0; i < sortlist.Count - 1; i++)
+                for (int i = 0; i < sortlist.Count - 1 - j; i++)
                 {
                     if (sortlist[i] > sortlist[i + 1])
                     {
@@ -25,17 +26,29 @@ namespace Bubblesort
                         listsorted = false;
                     }
                 }
+                j++;
             }
         }
         static void Main(string[] args)
         {
+            Random rand = new Random(0);
+
             List<int> numbs = new List<int> { 3, 1, 8, 2, 5 };
 
-            Bubblesort(numbs);
-            for (int i = 0; i < numbs.Count; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                Console.WriteLine(numbs[i]);
+                numbs.Add(rand.Next(0,50));
             }
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Bubblesort(numbs);
+            var elapsedms = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedms);
+
+            //for (int i = 0; i < numbs.Count; i++)
+            //{
+            //    Console.WriteLine(numbs[i]);
+            //}
 
         }
     }
